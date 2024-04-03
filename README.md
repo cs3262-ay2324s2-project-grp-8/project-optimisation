c2d220b5b0be30dfc43aac5c70b94dfbd666dd64
Chun Khai <>
2024-04-03 12:50:55 +0800

    Implemented Agent

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # project-optimisation
+
+useful files
+- agents_landmarks_multiagent.py (integrated with environment)
+- brain.py
+- dqn_agent.py
+- uniform_experience_replay.py (not sure what its for)
+- sum_tree.py (needed in uniform_experience_replay.py)
+
+useful parts in paper
+- 1.2 multi agent reinforcement learning
+- 2.1.1 Agent landmarks
+- 2.3, 2.4 good to understand
--- RLAgent/agent.py
+++ RLAgent/agent.py
@@ -1,23 +1,174 @@
+import numpy as np
+import random
+
 from utils import Worker, Node
+
+from prioritized_experience_replay import Memory as PER
+from uniform_experience_replay import Memory as UER
+from brain import Brain
+
+MAX_EPSILON, MIN_EPSILON = 1.0, 0.01
+MAX_BETA, MIN_BETA = 0.4, 1.0
 
 class AgentWorker(Worker):
     
-    def __init__(self, type, start, rate, timestamp):
+    epsilon = MAX_EPSILON
+    beta = MAX_BETA
+    
+    def __init__(self, arguments, type, start, rate, timestamp, bee_index, state_size, action_size, brain_name, arguments):
         super().__init__(type=type, start=start, rate=rate, timestamp=timestamp)
-        self.model = None # for now
+
+        self.bee_index = bee_index
+        self.state_size = state_size
+        self.action_size = action_size
+        self.brain = Brain(state_size, action_size, brain_name, arguments)
+        self.gamma = 0.95
+        self.memory_model = arguments['memory_model']
+        
+        if self.memory_model == 'PER':
+            self.memory = PER(arguments['memory_capacity'])
+        elif self.memory_model == 'UER':
+            self.memory = UER(arguments['memory_capacity'])
+        
+        self.target_type = arguments['target_type']
+        self.update_target_frequency = arguments['target_frequency']
+        self.max_exploration_step = arguments['maximum_exploration']
+        self.batch_size = arguments['batch_size']
+        self.step = 0
+        self.test = arguments['test']
+        if self.test:
+            self.epsilon = MIN_EPSILON
+        
         raise NotImplementedError
     
-    def observe(self):
-        raise NotImplementedError
+    def find_targets_per(self, batch):
+        batch_len = len(batch)
+
+        states = np.array([o[1][0] for o in batch])
+        states_ = np.array([o[1][3] for o in batch])
+
+        p = self.brain.predict(states)
+        p_ = self.brain.predict(states_)
+        pTarget_ = self.brain.predict(states_, target=True)
+
+        x = np.zeros((batch_len, self.state_size))
+        y = np.zeros((batch_len, self.action_size))
+        errors = np.zeros(batch_len)
+
+        for i in range(batch_len):
+            o = batch[i][1]
+            s = o[0]
+            a = o[1][self.bee_index]
+            r = o[2]
+            s_ = o[3]
+            done = o[4]
+
+            t = p[i]
+            old_value = t[a]
+            if done:
+                t[a] = r
+            else:
+                if self.target_type == 'DDQN':
+                    t[a] = r + self.gamma * pTarget_[i][np.argmax(p_[i])]
+                elif self.target_type == 'DQN':
+                    t[a] = r + self.gamma * np.amax(pTarget_[i])
+                else:
+                    print('Invalid type for target network!')
+
+            x[i] = s
+            y[i] = t
+            errors[i] = np.abs(t[a] - old_value)
+
+        return [x, y, errors]
+
+    def find_targets_uer(self, batch):
+        batch_len = len(batch)
+
+        states = np.array([o[0] for o in batch])
+        states_ = np.array([o[3] for o in batch])
+        
+        p = self.brain.predict(states)
+        p_ = self.brain.predict(states_)
+        pTarget_ = self.brain.predict(states_, target=True)
+
+        x = np.zeros((batch_len, self.state_size))
+        y = np.zeros((batch_len, self.action_size))
+        errors = np.zeros(batch_len)
+
+        for i in range(batch_len):
+            o = batch[i]
+            s = o[0]
+            a = o[1][self.bee_index]
+            r = o[2]
+            s_ = o[3]
+            done = o[4]
+
+            t = p[i]
+            old_value = t[a]
+            if done:
+                t[a] = r
+            else:
+                if self.target_type == 'DDQN':
+                    t[a] = r + self.gamma * pTarget_[i][np.argmax(p_[i])]
+                elif self.target_type == 'DQN':
+                    t[a] = r + self.gamma * np.amax(pTarget_[i])
+                else:
+                    print('Invalid type for target network!')
+
+            x[i] = s
+            y[i] = t
+            errors[i] = np.abs(t[a] - old_value)
+
+        return [x, y]
+    
+    def greedy_move(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+    
+    def observe(self, sample):
+        if self.memory_model == 'UER':
+            self.memory.remember(sample)
+        elif self.memory_model == 'PER':
+            _, _, errors = self.find_targets_per([[0, sample]])
+            self.memory.remember(sample, errors[0])
     
     def decay_epsilon(self):
-        raise NotImplementedError
+        self.step += 1
+
+        if self.test:
+            self.epsilon = MIN_EPSILON
+            self.beta = MAX_BETA
+        else:
+            if self.step < self.max_exploration_step:
+                self.epsilon = MIN_EPSILON + (MAX_EPSILON - MIN_EPSILON) * (self.max_exploration_step - self.step)/self.max_exploration_step
+                self.beta = MAX_BETA + (MIN_BETA - MAX_BETA) * (self.max_exploration_step - self.step)/self.max_exploration_step
+            else:
+                self.epsilon = MIN_EPSILON
     
     def replay(self):
-        raise NotImplementedError
-    
+        if self.memory_model == 'UER':
+            batch = self.memory.sample(self.batch_size)
+            x, y = self.find_targets_uer(batch)
+            self.brain.train_model(x, y)
+        
+        elif self.memory_model == 'PER':
+            [batch, batch_indices, batch_priorities] = self.memory.sample(self.batch_size)
+            x, y, errors = self.find_targets_per(batch)
+
+            normalized_batch_priorities = [float(i) / sum(batch_priorities) for i in batch_priorities]
+            importance_sampling_weights = [(self.batch_size * i) ** (-1 * self.beta)
+                                           for i in normalized_batch_priorities]
+            normalized_importance_sampling_weights = [float(i) / max(importance_sampling_weights)
+                                                      for i in importance_sampling_weights]
+            sample_weights = [errors[i] * normalized_importance_sampling_weights[i] for i in xrange(len(errors))]
+
+            self.brain.train_model(x, y, np.array(sample_weights))
+
+            self.memory.update(batch_indices, errors)
+
     def update_target_model(self):
-        raise NotImplementedError
+        if self.step % self.update_target_frequency == 0:
+            self.brain.update_target_model()
     
     def move_back_to_origin(self, origin: Node):
         self.move(origin)
