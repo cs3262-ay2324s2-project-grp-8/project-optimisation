81540a7a732eee6383fdc30725e92446a6ed7187
Tan Kai Min Russell <russelltanreg38@gmail.com>
2024-04-08 00:23:49 +0800

    Add MCTS

---
+++ mcts/treerun.py
@@ -0,0 +1,192 @@
+from mcts import MonteCarlo
+from node import TreeNode
+from actions import move_immutable, step_state
+from reward import calculate_reward_and_best_action
+from utils import Graph, Node
+from itertools import product
+import numpy as np
+from copy import deepcopy
+
+filename : str = "../graphs/graph1.json"
+graph = Graph(filename)
+timestamp : int = 0
+origin = graph.get_Origin()
+
+'''
+State Representation:
+[W1, W2, W3, W4, W5, W6, W7, W8, W9, rewards_extracted, budget_used, budget_left, S1, S2, ... , S9]
+W_{i} = (x, y, type, is_hired, is_fired, is_extracting, extraction_time_left, timestamp)
+S_{i} = (x, y, type, is_sighted, accessed)
+
+Actions:
+
+0 - Move North
+1 - Move South
+2 - Move East
+3 - Move West
+4 - Move NE
+5 - Move NW
+6 - Move SE
+7 - Move SW
+8 - Hire
+9 - Extract
+10 - Don’t Move (Idle for those that are not hired)
+11 - Fire
+'''
+
+# WORKER Accessors:
+X = 0
+Y = 1
+TYPE = 2
+IS_HIRED = 3
+IS_FIRED_BEFORE = 4
+IS_EXTRACTING = 5
+EXTRACT_TIME_LEFT = 6
+TIMESTAMP = 7
+
+# SITE Accessors
+IS_SIGHTED = 3
+ACCESSED = 4
+
+MOVE_NORTH = 0
+MOVE_SOUTH = 1
+MOVE_EAST = 2
+MOVE_WEST = 3
+MOVE_NORTH_EAST = 4
+MOVE_NORTH_WEST = 5
+MOVE_SOUTH_EAST = 6
+MOVE_SOUTH_WEST = 7
+HIRE = 8
+EXTRACT = 9
+IDLE = 10
+FIRE = 11
+
+
+INITIAL_BUDGET = 10000
+
+state = list()
+
+# Fill up the initial state
+for i in range(1, 10):
+    if (1 <= i <= 3):
+        type_worker = 1
+    elif (i in [4, 5]):
+        type_worker = 2
+    else:
+        type_worker = 3
+    state.append([origin.get_x_coordinate(), origin.get_y_coordinate(), type_worker, False, False, False, 0, 1])
+
+state.extend([0, 0, INITIAL_BUDGET])
+
+type_1_sites = graph.retrieve_all_sites_of_type(2)
+type_2_sites = graph.retrieve_all_sites_of_type(3)
+type_3_sites = graph.retrieve_all_sites_of_type(4)
+
+for site in type_1_sites:
+    state.append([site.get_x_coordinate(), site.get_y_coordinate(), 1, False, False])
+for site in type_2_sites:
+    state.append([site.get_x_coordinate(), site.get_y_coordinate(), 2, False, False])
+for site in type_3_sites:
+    state.append([site.get_x_coordinate(), site.get_y_coordinate(), 3, False, False])
+
+def print_state(state):
+    for i, worker in enumerate(state):
+        if (i < 9):
+            print(f"Worker {i + 1}'s state : {worker}")
+        elif (i == 9):
+            print(f"Rewards Extracted So Far : {state[9]}")
+        elif (i == 10):
+            print(f"Budget Used : {state[10]}")
+        elif (i == 11):
+            print(f"Budget Left : {state[11]}")
+        else:
+            print(f"Reward Site : {state[i]}")
+
+print_state(state)
+
+def floyd_warshall(graph: Graph) :
+        vertices = graph.get_vertices()
+        edges = graph.get_edges()
+        ssp = dict()
+        for v1 in vertices.keys():
+            for v2 in vertices.keys():
+                if (v1 not in ssp.keys()):
+                    ssp[v1] = dict()
+                if (v2 not in ssp.keys()):
+                    ssp[v2] = dict()
+                if v1 == v2 :
+                    ssp[v1][v2] = 0
+                    continue
+                ssp[v1][v2] = 1 if (v2 in edges[v1]) else np.inf
+                ssp[v2][v1] = 1 if (v1 in edges[v2]) else np.inf
+        for k in vertices.keys():
+            for i in vertices.keys():
+                for j in vertices.keys():
+                    ssp[i][j] = min(ssp[i][j], ssp[i][k] + ssp[k][j])
+        assert(len(vertices.keys()) == len(ssp.keys()))
+        # for i in vertices.keys():
+        #     print("===========================")
+        #     for j in vertices.keys():
+        #         print(f"{i} -> {j} : {ssp[i][j]}")
+        return ssp
+
+ssp = floyd_warshall(graph=graph)
+
+current_state = TreeNode(state)
+
+montecarlo: MonteCarlo = MonteCarlo(current_state)
+
+# child finder
+def child_finder(node : TreeNode, montecarlo : MonteCarlo):
+    state : list = node.state
+    moves : list = list()
+    for worker_idx in range(9):
+        w = state[worker_idx]
+        if (not w[IS_HIRED]):
+            moves.append([HIRE, IDLE])
+        else :
+            if (w[IS_EXTRACTING] and w[EXTRACT_TIME_LEFT] > 0):
+                moves.append([EXTRACT])
+            else:
+                curr_node:Node = graph.get_Node(w[X], w[Y])
+                if (curr_node.get_reward() > 0 and curr_node.get_type() - 1 <= w[TYPE]):
+                    # accessible reward note
+                    rwd_site = None
+                    for reward_idx in range(12, 12 + 9):
+                        site = state[reward_idx]
+                        if (site[X] == w[X] and site[Y] == w[Y]):
+                            rwd_site = site
+                            break
+                    if (rwd_site[ACCESSED]):
+                        continue
+                    else :
+                        moves.append([EXTRACT])
+                else :
+                    # Not a reward node
+                    adj_nodes: list = graph.get_adjacent_nodes_by_coordinates(w[X], w[Y])
+                    movement_actions = []
+                    for next_node in adj_nodes:
+                        profit, action = calculate_reward_and_best_action(state, (w[X], w[Y]), (next_node[X], next_node[Y]), ssp, w[TYPE])
+                        movement_actions.append((action, profit))
+                    sorted(movement_actions, reverse=True, key=lambda x : x[1])
+                    if (len(movement_actions) == 0):
+                        moves.append([FIRE])
+                    elif (movement_actions[0][1] < 0):
+                        moves.append([FIRE])
+                    else :
+                        moves.append([movement_actions[0][0], movement_actions[1][0]])
+    movement_combinations = list(product(*moves))
+    for move_combi in movement_combinations:
+        node.add_child(step_state(state, move_combi))
+                    
+
+    raise NotImplementedError
+
+
+# node evaluator
+def node_evaluator(node, montecarlo):
+    pass
+
+
+for timestamp in range(1, 21):
+    pass
---
+++ mcts/utils.py
@@ -0,0 +1,296 @@
+import json, re
+
+DEBUG = False
+LOG_FULL = False
+LOG_TIME_TAKEN = False
+LOG_EXTRACTION = False
+LOG_PROFIT = True
+
+BASIC_SITE = 0
+ORIGN      = 1
+SITE1      = 2
+SITE2      = 3
+SITE3      = 4
+
+TYPE_ID_TO_STRING_TYPE: dict[int, str] = {
+    0 : "BASIC SITE",
+    1 : "ORIGIN",
+    2 : "REWARD TYPE 1",
+    3 : "REWARD TYPE 2",
+    4 : "REWARD TYPE 3"
+}
+class Node:
+
+    def __init__(self, node_info, coordinate):
+        self.type = node_info["vertex_type"]
+        self.reward = node_info["reward"]
+        self.current_reward = node_info["reward"]
+        self.active_period = node_info["mult_time_active"]
+        self.acquire_time = node_info["time_to_acquire"]
+        self.coordinate = coordinate
+        self.accessed = False
+        self.current_accessors = []
+        self.sighted = False
+        self.extracted = False
+        self.extracted_before = False
+        self.extractor : Node | None = None
+    
+    def __repr__(self) -> str:
+        return f'({self.get_x_coordinate(), self.get_y_coordinate()} TYPE : {TYPE_ID_TO_STRING_TYPE[self.type]})'
+
+    def get_type(self):
+        '''
+        Basic               := 0
+        Origin              := 1
+        Site1, Site2, Site3 := 2, 3, 4
+        '''
+        return self.type
+
+    def get_coordinate(self):
+        return self.coordinate
+
+    def get_active_period(self) -> list :
+        return self.active_period
+    
+    def someone_eyeing_node(self):
+        return self.sighted
+    
+    def sight(self):
+        self.sighted = True
+    
+    def get_acquire_time(self):
+        return self.acquire_time
+    
+    def enter(self, agent_worker):
+        self.current_accessors.append(agent_worker)
+
+    def leave(self, agent_worker):
+        self.current_accessors.remove(agent_worker)
+
+    def can_extract(self):
+        return not self.extracted_before and not self.extracted and not self.sighted
+
+    def extract(self, agent_worker):
+        self.enter(agent_worker=agent_worker)
+        self.extractor = agent_worker
+        self.extracted = True
+
+    def has_extractor(self):
+        return self.extractor is not None
+    
+    def leave_node_extractor(self):
+        self.leave(self.extractor)
+        self.extractor = None
+        
+    def reset_node(self):
+        self.current_accessors.clear()
+        self.extractor = None
+        self.current_reward = self.reward
+        self.extracted = False
+        self.accessed = False
+        self.sighted = False
+
+    def access(self):
+        self.accessed = True
+
+    def is_accessed_before(self):
+        return self.accessed
+
+    def get_coordinate(self):
+        return self.coordinate
+    
+    def get_x_coordinate(self):
+        return self.coordinate[0]
+    
+    def get_y_coordinate(self):
+        return self.coordinate[1]
+    
+    def get_reward(self):
+        return self.reward
+    
+    def zero_reward(self):
+        self.current_reward = 0
+        self.accessed = True
+
+
+class Graph:
+
+    def __init__(self, json_filename):
+        self.filename = re.search(r'graph(\d+)\.json', json_filename).group(1)
+        self.data = json.load(open(json_filename))
+        self.vertices = dict() # key (x1, y1) -> value : Node
+        self.edges = dict() # key (x1, y1) -> value : list of int tuples
+        self.workers_cost_rate: list[float] = [100.0, 200.0, 500.0]
+        self.site_type_rewards: list[float] = [0, 0, 0, 0, 0]
+        self.sites_info = dict() # key : site type -> value: list of Node objects
+        self.process()
+    
+    def __str__(self) -> str:
+        stuff =  {
+            "vertices": self.vertices,
+            "edges" : self.edges
+        }
+        string_construct = "Origin : " + str(self.get_Origin()) + "\n"
+        for e, v in stuff["edges"].items():
+            string_construct += str(e) + str(v) + "\n"
+        string_construct += "Type 1 Reward Sites: \n"
+        for k in self.retrieve_all_sites_of_type(SITE1):
+            string_construct += str(k) + "\n"
+        string_construct += "\n"
+        string_construct += "Type 2 Reward Sites: \n"
+        for k in self.retrieve_all_sites_of_type(SITE2):
+            string_construct += str(k) + "\n"
+        string_construct += "\n"
+        string_construct += "Type 3 Reward Sites: \n"
+        for k in self.retrieve_all_sites_of_type(SITE3):
+            string_construct += str(k) + "\n"
+        string_construct += "\n"
+        return string_construct
+
+    def process(self):
+
+        # Process the vertices
+        vertices_data = self.data["vertices"]
+        for x_coordinate in vertices_data:
+            for y_coordinate in vertices_data[x_coordinate]:
+                x_coordinate, y_coordinate = int(x_coordinate), int(y_coordinate)
+                node = Node(vertices_data[str(x_coordinate)][str(y_coordinate)], (x_coordinate, y_coordinate))
+                self.vertices[(x_coordinate, y_coordinate)] = node
+                if (node.get_type() > 1):
+                    self.site_type_rewards[node.get_type() - 1] = node.get_reward()
+                if (node.get_type() not in self.sites_info.keys()):
+                    self.sites_info[node.get_type()] = [node]
+                else:
+                    self.sites_info[node.get_type()].append(node)
+
+        # Process the edges
+        edges_data = self.data["edges"]
+        for edge in edges_data:
+            x1, y1, x2, y2 = edge
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            if ((x1, y1) not in self.edges.keys()):
+                self.edges[(x1, y1)] = [(x2, y2)]
+            else:
+                self.edges[(x1, y1)].append((x2, y2))
+            if ((x2, y2) not in self.edges.keys()):
+                self.edges[(x2, y2)] = [(x1, y1)]
+            else:
+                self.edges[(x2, y2)].append((x1, y1))
+            
+        print("Graph Initialised Successfully!")
+
+    def get_edges(self):
+        return self.edges
+
+    def get_vertices(self):
+        return self.vertices
+    
+    def get_Node(self, x : int, y: int):
+        return self.vertices[(x, y)]
+    
+    def get_adjacent_nodes_by_coordinates(self, x : int, y : int):
+        node: Node = self.vertices[(x, y)]
+        return self.get_adjacent_nodes_by_Node(node)
+
+    def get_adjacent_nodes_by_Node(self, node: Node):
+        neighbours = self.edges[node.get_coordinate()]
+        return [self.vertices[neighbour] for neighbour in neighbours]
+    
+    def get_site_reward(self, type: int):
+        return self.site_type_rewards[type]
+    
+    def retrieve_all_sites_of_type(self, type : int):
+        return self.sites_info[type]
+    
+    def get_worker_cost(self, type : int):
+        return self.workers_cost_rate[type - 1]
+    
+    def get_Origin(self):
+        return self.retrieve_all_sites_of_type(ORIGN)[0]
+    
+    def is_reward_site_by_node(self, node : Node):
+        return node.get_type() > 0
+    
+    def is_reward_site(self, x : int, y : int):
+        return self.is_reward_site_by_node(self.get_Node(x, y))
+    
+class Worker:
+
+    def __init__(self, type : int, start, rate, timestamp):
+        self.type = type
+        self.location = start
+        self.rate = 500 if type == 3 else type * 100
+        self.ts = timestamp
+        self.is_Hired = False
+        self.isExtracting = False
+        self.fired = False
+        self.waitTime = 0
+
+    def get_type(self):
+        return self.type
+
+    def get_location(self):
+        return self.location
+    
+    def get_rate(self):
+        return self.rate
+    
+    def move(self, next_node: Node):
+        self.location = next_node.get_coordinate()
+
+    def move_to_coordinates(self, x, y):
+        self.location = (x, y)
+
+    def get_timestamp(self):
+        return self.ts
+    
+    def hire(self):
+        self.is_Hired = True
+
+    def fire(self):
+        self.is_Hired = False
+        self.fired = True
+        
+    def fire(self):
+        self.is_Hired = False
+
+    def extract(self, node : Node):
+        self.isExtracting = True
+        self.waitTime = node.get_type() - 1
+        node.extract(self)
+
+    def done_extracting(self):
+        self.isExtracting = False
+
+    def is_extracting(self):
+        return self.isExtracting
+
+    def decrease_waitTime(self):
+        self.waitTime -= 1
+
+    def get_waitTime(self):
+        return self.waitTime
+        
+    def isHired(self):
+        return self.is_Hired
+
+    def isFiredBefore(self):
+        return self.fired
+    
+    def reward_at_location(self, graph, zero_out):
+        node = graph.get_Node(self.location[0], self.location[1])
+        reward = node.get_reward()
+        if (zero_out):
+            node.zero_reward()
+        return reward
+
+
+# if __name__ == "__main__" :
+#     graph = Graph("Test_Graph_Slightly_Off_the_Beaten_Path/Test_Graph_Slightly_Off_the_Beaten_Path.json")
+#     # print(graph.get_vertices())
+#     ORIGIN = 1
+#     print(graph.get_adjacent_nodes_by_coordinates(5,5))
+#     print(graph.get_edges())
+#     print(graph.retrieve_all_sites_of_type(ORIGIN))
+#     print(len(graph.get_vertices()))
+#     print(len(graph.get_edges()))
